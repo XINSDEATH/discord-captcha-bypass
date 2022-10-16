6f3f361de55a9e52ead291ee3231d821b3dd632c
XIN <88397521+XINSDEATH@users.noreply.github.com>
2022-10-16 15:10:54 +0300

    Create bypass.py

---
+++ bypass.py
@@ -0,0 +1,26 @@
+
+for i in range(15):
+ try:
+                createTask = httpx.post(            "https://api.capmonster.cloud/createTask", json={
+                        "clientKey": settings["capmonsterKey"],     "task": {
+                            "type": "HCaptchaTaskProxyless",         "websiteURL": "https://discord.com/channels/@me",         "websiteKey":
+                            "4c672d35-0701-42b2-88c3-78380b0db560"
+                        }
+                    }).json()["taskId"]
+
+                print(fade.pinkred(f"Captcha Task: {createTask}"))
+
+                getResults = {}
+                getResults["status"] = "processing"
+                while getResults["status"] == "processing":
+                    getResults = httpx.post(                "https://api.capmonster.cloud/getTaskResult",     json={
+                            "clientKey": settings["capmonsterKey"],         "taskId": createTask
+                        }).json()
+
+                    time.sleep(1)
+
+                solution = getResults["solution"]["gRecaptchaResponse"]
+
+                print(fade.pinkred(f"Captcha Solved"))
+ except:
+                pass
